3a1eeac2cacfe8f1a9609df0affde1a6ab075506
Harsh Singh <harsingh@cimpress.com>
2017-01-20 21:37:04 -0500

    optimize for krsna

--- bhajans.js
+++ bhajans.js
@@ -1,52 +1,53 @@
-var app;
+var app
 
-app = angular.module("bhajans", ["ngMaterial"]);
+app = angular.module('bhajans', ['ngMaterial'])
 
-app.config(function($sceProvider, $mdThemingProvider) {
-  $sceProvider.enabled(false);
+app.config(function ($sceProvider, $mdThemingProvider) {
+  $sceProvider.enabled(false)
   $mdThemingProvider.theme('default')
     .primaryPalette('orange')
-    .accentPalette('pink');
-});
+    .accentPalette('pink')
+})
 
-app.filter("highlight", function() {
-  return function(text, search, caseSensitive) {
+app.filter('highlight', function () {
+  return function (text, search, caseSensitive) {
     if (text && (search || angular.isNumber(search))) {
-      text = text.toString();
-      search = search.toString();
+      text = text.toString()
+      search = search.toString()
       if (caseSensitive) {
-        return text.split(search).join("<span class=\"ui-match\">" + search + "</span>");
+        return text.split(search).join('<span class="ui-match">' + search + '</span>')
       } else {
-        return text.replace(new RegExp(search, "gi"), "<span class=\"ui-match\">$&</span>");
+        return text.replace(new RegExp(search, 'gi'), '<span class="ui-match">$&</span>')
       }
     } else {
-      return text;
+      return text
     }
-  };
-});
+  }
+})
 
-app.controller("BhajanSearchCtrl", [
-  "$scope", "$http", function(_, $http) {
+app.controller('BhajanSearchCtrl', [
+  '$scope', '$http', function (_, $http) {
     _.query = ''
-    function makeSearchable(line) {
-      if(! (line && line.replace)){
+    function makeSearchable (line) {
+      if (!(line && line.replace)) {
       }
       return line
-        .replace('h','')
-        .replace(/a+/g,'a')
-        .replace(/[iey]+/g,'iey')
-        .replace(/[uo]+/g,'uo')
-        .replace(/[tdl]/g,'tdl')
-        .replace('z','r')
+        .replace('krs', 'kris')
+        .replace('h', '')
+        .replace(/a+/g, 'a')
+        .replace(/[iey]+/g, 'iey')
+        .replace(/[uo]+/g, 'uo')
+        .replace(/[tdl]/g, 'tdl')
+        .replace('z', 'r')
     }
-    $http.get("bhajan-index.json").success(function(data) {
+    $http.get('bhajan-index.json').success(function (data) {
       _.bhajans = data.map(function (line) {
-        return {bhajan:line, search:makeSearchable(line)}
+        return {bhajan: line, search: makeSearchable(line)}
       })
-    });
-    _.closeMatch = function(actual) {
+    })
+    _.closeMatch = function (actual) {
       var searched = makeSearchable(_.query.toLowerCase())
       return !!actual.search.match(searched)
     }
   }
-]);
+])
